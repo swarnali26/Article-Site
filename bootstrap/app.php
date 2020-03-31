0e41347cc8e45c5eb50be9eaa06098bf60a55c7a
swarnali <swarnali.marik@gmail.com>
2020-03-31 14:23:15 +0530

    article

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -23,11 +23,11 @@ $app = new Laravel\Lumen\Application(
     dirname(__DIR__)
 );
 
-$app->withFacades();
+//$app->withFacades();
 
 $app->withEloquent();
 
-$app->configure('swagger-lume');
+//$app->configure('swagger-lume');
 /*
 |--------------------------------------------------------------------------
 | Register Container Bindings
@@ -95,7 +95,7 @@ $app->routeMiddleware([
 $app->register(App\Providers\AppServiceProvider::class);
 $app->register(App\Providers\AuthServiceProvider::class);
 $app->register(App\Providers\EventServiceProvider::class);
-$app->register(SwaggerLume\ServiceProvider::class);
+//$app->register(SwaggerLume\ServiceProvider::class);
 
 
 /*
